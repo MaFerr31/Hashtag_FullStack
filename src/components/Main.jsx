6b3bf67e8a0e06054608e061c3f076cf9cdc4e7e
Mariana Ferreira <mariana.ferreira@cromai.com>
2025-02-12 15:35:30 -0300

    refact: separando componentes.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+import ItemList from './ItemList'
 
 const Main = () => {
   return (
     <div>
       <div className="main">
-        <div className="item-list">
-            <div className="item-list__header">
-                <h2 className="item-list__title">Artistas Populares</h2>
-                <a href="/" className="item-list__link">Mostrar tudo</a>
-            </div>
+        <ItemList title="Artistas" items={10}/>
 
-            <div className="single-item">
-                <div className="single-item__div-image-button">
-                    <div className="single-item__div-image">
-                        <img src="http://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista" 
-                        className="single-item__image" />
-                    </div>
-                    <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
-                </div>
-                <p>Henrique e Juliano</p>
-                <p>Artista</p>
-            </div>
-        </div>
+        <ItemList title="Músicas" items={20}/> 
       </div>
     </div>
   )
