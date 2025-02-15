332bfff30d72f3df8c89eb52a701a0e241b749d8
Mariana Ferreira <mariana.ferreira@cromai.com>
2025-02-15 12:15:45 -0300

    feat: singleItem component with logic

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
-const SingleItem = () => {
+const SingleItem = ({ id, name, image, banner, artist, idPath }) => {
   return (
-    <div className="single-item">
+    <Link to={`${idPath}/${id}`} className="single-item">
         <div className="single-item__div-image-button">
             <div className="single-item__div-image">
-                <img src="http://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista" 
+                <img src={image} alt={`Imagem do Artista ${name}`}
                 className="single-item__image" />
             </div>
             <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
         </div>
         <div className="single-item__texts">
-            <p className='single-item__title'>Henrique & Juliano</p>
-            <p className='single-item__type'>Artista</p>
+            <p className='single-item__title'>{name}</p>
+            <p className='single-item__type'>{artist ?? "Artista"}</p>
         </div>
-    </div>
+    </Link>
   )
 }
 
