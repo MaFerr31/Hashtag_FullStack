6bc74eb72d8881f7b1d166aa6540541efd89e4b9
Mariana Ferreira <mariana.ferreira@cromai.com>
2025-02-15 12:13:20 -0300

    refact: New Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import logoSpotify from '../assets/logo/spotify-logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header'>
-      <img src={logoSpotify} alt="Logo do Spotify" />
-
+      <Link to='/'><img src={logoSpotify} alt="Logo do Spotify" /></Link>
       <h1>
-        <a className='header__link' href="/">Spotify</a>
+        <Link to="/" className='header__link'>Spotify</Link>
       </h1>
     </div>
   )
